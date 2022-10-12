768daa65c2eb7e4a994ca3d706a502f8f6a57917
Faryar <faryalmemon251@gmail.com>
2022-10-12 14:51:19 +0530

    Update flasker

--- hello.py
+++ hello.py
@@ -1,9 +1,19 @@
 # http://127.0.0.1:5000
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 
 # Create a Flask instance
 app = Flask(__name__)
+app.debug = True
+app.config['SECRET_KEY'] = "my super secret key that no one is supposed to know!!"
+
+# create a form class
+class NamerForm(FlaskForm):
+    name = StringField("What's your name?", validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 
 # jinja2 filters: safe, upper, lower, capitalize, trim, splitags
@@ -41,4 +51,24 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+
+# Create Name Page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    # Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash("Form Submitted Successfully!")
+
+    return render_template('name.html', 
+            name=name, 
+            form=form)
+
+
+
+
 
---
+++ static/js/myfile.js
@@ -0,0 +1,1 @@
+document.getElementById('demo').innerHTML = '<a href="https://www.freepik.com/free-vector/blogging-illustration-concept_6192594.htm#query=content%20writer&position=3&from_view=keyword">Image by storyset</a> on Freepik';
